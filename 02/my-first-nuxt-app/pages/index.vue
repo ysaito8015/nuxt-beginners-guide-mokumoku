8bdbb87e6ee99a0e5f0b9f155da53b62ae7728a1
Yusuke_Saito <ysaito8015@gmail.com>
2022-09-16 18:13:42 +0200

    cange mounted() to asyncData()

--- 02/my-first-nuxt-app/pages/index.vue
+++ 02/my-first-nuxt-app/pages/index.vue
@@ -1,15 +1,65 @@
 <template>
-  <div>
-    <!-- -->
-  </div>
+  <section class="container">
+    <div class="block">
+      <h3>Nuxt.js のタグがつけられた投稿の一覧</h3>
+      <ul>
+        <li
+          v-for="item in items"
+          :key="item.id"
+          >
+          <h4>
+            <span>
+              {{ item.title }}
+            </span>
+            <small>
+              by {{ item.user.id }}
+            </small>
+          </h4>
+          <div class="item-body">
+            {{ item.body.slice(0, 130) }} ......
+          </div> <!-- end of item-body -->
+          <p>
+            <a
+              :href="item.url"
+            >
+              {{ item.url }}
+            </a>
+          </p>
+        </li>
+      </ul>
+    </div> <!-- end of block -->
+  </section> <!-- end of container -->
 </template>
 
 <script>
 export default {
-  async mounted() {
-    console.log(
-      JSON.stringify(await this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js'), true, ' ')
-      )
+  async asyncData({ app }) {
+    const items = await
+      app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+    return {
+      items
+    }
   }
 }
 </script>
+
+<style>
+.container {
+  min-height: 100vh;
+  padding: 16px;
+}
+
+h3 {
+  margin: 16px 0;
+  padding: 8px 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+
+li + li {
+  margin: 16px 0;
+}
+
+p {
+  margin: 8px 0;
+}
+</style>
