54dc931375c30518621e033d66544950c433e2e1
Yusuke_Saito <ysaito8015@gmail.com>
2022-09-19 16:05:12 +0200

    add a layout component

---
+++ 03/nuxt-api-examples/layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <span>
+      default layout
+    </span>
+    <AppNabigation />
+    <hr>
+    <nuxt />
+    <hr>
+    <footer>
+      footer
+    </footer>
+  </div>
+</template>
+
+
+<script>
+import AppNavigation from '~/components/AppNavigation.vue'
+
+export default {
+  components: {
+    AppNavigation
+  }
+}
+</script>
