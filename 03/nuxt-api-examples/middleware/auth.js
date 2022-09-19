76d3c0feeeeeb024a675f34640cd1f1b559239d8
Yusuke_Saito <ysaito8015@gmail.com>
2022-09-19 22:06:29 +0200

    add routing

--- 03/nuxt-api-examples/middleware/auth.js
+++ 03/nuxt-api-examples/middleware/auth.js
@@ -1,7 +1,17 @@
-export default () => {
-  if (process.browser) {
-    console.log('console.log() on browser')
-  } else {
-    console.log('console.log() on SSR server')
+import Cookies from 'universal-cookie'
+
+export default ({ req, route, redirect}) => {
+  console.log(route.path)
+  if (['/'].includes(route.path)) {
+    return
+  }
+  const cookies = req ? new Cookies(req.headers.cookie) : new Cookies()
+  const credential = cookies.get('credential')
+
+  if (credential && route.path === '/login') {
+    return redirect('/')
+  }
+  if (!credential && route.path !== '/login') {
+    return redirect('/login')
   }
 }
