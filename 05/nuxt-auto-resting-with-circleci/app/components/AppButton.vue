5de7c376b3cf17680e8501d351471177b819d889
Yusuke_Saito <ysaito8015@gmail.com>
2022-10-05 01:15:52 +0200

    [ch05] add snapshot test to component

---
+++ 05/nuxt-auto-resting-with-circleci/app/components/AppButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p
+      v-if="clicked"
+      >
+      clicked
+    </p>
+    <button
+      type="button"
+      @click="handleClick"
+      >
+      click
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clicked: false
+    }
+  },
+  methods: {
+    handleClick() {
+      this.clicked = true
+    }
+  }
+}
+</script>
